34dc4b191a4479f519e98f4712ca827f6c93cdf0
Oclecio Alves <oclecioalvessousa@gmail.com>
2023-08-04 21:52:10 -0300

    arrumando texto do botão

--- components/Footer.js
+++ components/Footer.js
@@ -75,7 +75,7 @@ const Footer = () => {
             ></textarea>
           </div>
           <button class="text-white bg-pink-400 border-0 py-2 px-6 focus:outline-none hover:bg-pink-400 rounded text-lg">
-            Button
+            Enviar
           </button>
         </div>
       </div>
